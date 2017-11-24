443586fb00be090a08ca3fe49d1833dc8401251d
darkglimmer <2392897209@qq.com>
2017-11-24 15:02:58 +0800

    task3

--- week5/task3.js
+++ week5/task3.js
@@ -1,45 +1,20 @@
 function parseQuery(query){
-    for(var i=0;i<query.length;i++){
-      if(query[i]=='&'&&i==(query.length-1)){
-        query[i]=":\"\"";
-      }
-      else{
-        if(query[i]=='='){
-          query[i]=":\"";
-        }
-        else if(query[i]=='&'){
-          query[i]="\",";
-        }
-      }
-    }
-    document.write("{");
-    for(var j=0;j<query.length;j++){
-      document.write(query[i]);
-    }
-    document.write("}");
+  query+="\"";
+  if(query[query.length]=="&"){
+    a=query.substring(0,query.length-1);
+    b=a+":\"\"";
   }
-   var jerry = parseQuery("name=jerry&age=1");
-   var tom = parseQuery("name=tom&age=12&gender&");
+  else{
+    b=query;
+  }
+  s=b.replace(/=/g, ":\"");
+  ss=(s.replace(/&/g,"\"&")).split("&"); 
+  document.write(ss);
+  document.write("<br>");
+}
 
 
 
 
-
-// function parseQuery(query){
-//   var a=query.split("&");
-//   for(var i in a){
-//     if(a[i]=="="){
-//       a[i]=":'";
-//       document.write(a[i]);
-//       }
-//     else{
-//        document.write(a[i]);
-//     }
-//   }
-//   document.write("<br>")
-// }
-//  var jerry = parseQuery("name = jerry & age = 1");
-//  jerry;
-//  var tom = parseQuery("name = tom & age = 12 & gender &");
-//  tom;
-    
+var jerry = parseQuery("name=jerry&age=1");
+var tom = parseQuery("name=tom&age=12&gender&");
